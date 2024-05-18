aba232787234f7b63cfb79ed38d240be1ad93236
daminoworld <ekadls757@gmail.com>
2024-05-18 13:58:48 +0900

    Add: Xcode project files

---
+++ Dorae/Dorae/DoraeApp.swift
@@ -0,0 +1,32 @@
+//
+//  DoraeApp.swift
+//  Dorae
+//
+//  Created by Damin on 5/18/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct DoraeApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
