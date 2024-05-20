d65829ed11254111061e1d0a1a60a62e151c0fb1
jiyoungsophia <sophiathedev@gmail.com>
2024-05-20 20:27:54 +0900

    Chore: update Knot Model

--- Dorae/Dorae/Model/Knot.swift
+++ Dorae/Dorae/Model/Knot.swift
@@ -14,13 +14,13 @@ enum 매듭 {
 }
 
 struct 기본 {
-    let 매듭이름: String
+    let 매듭이름: 기본매듭이름
     let 매듭이미지: String
     var 귀: [String] = []
 }
 
 struct 응용 {
-    let 매듭이름: String
+    let 매듭이름: 응용매듭이름
     let 매듭이미지: String
     var 하위매듭리스트: [String] = []
 }
@@ -36,4 +36,38 @@ enum 끈목 {
 enum 술 {
     case 딸기술, 봉술, 방망이술, 낙지발술, 방울술, 전복술, 금전지술, 잔술
 
+}
+
+enum 기본매듭이름: String {
+    case 도래매듭
+    case 귀도래매듭
+    case 연봉단추매듭
+    case 가락지매듭
+    case 생쪽매듭
+    case 나비매듭
+    case 매화매듭
+    case 국화매듭
+    case 병아리매듭
+    case 잠자리매듭
+    case 동심결매듭
+    case 안경매듭
+    case 장구매듭
+    case 가지방석매듭
+    case 귀달린가지방석매듭
+    case 딸기매듭
+    case 석씨매듭
+    case 꼰디기매듭
+    case 매미매듭
+    case 사색판매듭
+}
+
+enum 응용매듭이름: String {
+    case 항아리매듭
+    case 지게매듭
+    case 왕비국화매듭
+    case 육립매듭
+    case 꽃육립매듭
+    case 공작매듭
+    case 쌍공작매듭
+    case 스타매듭
 }
