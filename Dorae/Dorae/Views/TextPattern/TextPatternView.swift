c8bed4c148177ea31dbf86e3ee9e383dcb7914e7
ye-gang <yegang13209@naver.com>
2024-05-21 02:18:12 +0900

    Chore: #9-패턴뷰, 텍스트 패턴뷰 수정

--- Dorae/Dorae/Views/TextPattern/TextPatternView.swift
+++ Dorae/Dorae/Views/TextPattern/TextPatternView.swift
@@ -8,8 +8,77 @@
 import SwiftUI
 
 struct TextPatternView: View {
+    struct ListItem: Identifiable, Hashable {
+        var id = UUID()
+        var name: String
+        var imgName: String
+        var ear: [String]?
+    }
+    
+    @State var sharedData: [ListItem] = [
+        ListItem(name: 도래매듭.매듭이름.rawValue, imgName: 도래매듭.매듭이름.rawValue, ear: nil),
+        ListItem(name: 귀도래매듭.매듭이름.rawValue, imgName: 귀도래매듭.매듭이름.rawValue, ear: [""]),
+        ListItem(name: 단추매듭.매듭이름.rawValue, imgName: 단추매듭.매듭이름.rawValue, ear: nil),
+        ListItem(name: 가락지매듭.매듭이름.rawValue, imgName: 가락지매듭.매듭이름.rawValue, ear: nil)
+    ]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            NavigationStack {
+                List {
+                    Section(content: {
+                        ForEach(sharedData) { item in
+                            HStack {
+                                Image(systemName: "mic.fill")
+                                Text(item.name)
+                            }
+                        }
+                        .onDelete(perform: removeList)
+                        .onMove(perform: moveList)
+                    }, header: {
+                        HStack {
+                            Text("글 도안")
+                                .font(.title)
+                                .bold()
+                                .foregroundStyle(.black)
+                            
+                            Spacer()
+                            
+                            Text("이동/삭제")
+                                .font(.subheadline)
+                                .bold()
+                                .foregroundStyle(.red)
+                        }
+                        .padding(.bottom, 23)
+                        
+                    })
+                }
+                .listStyle(.plain)
+            }
+            
+            Button {
+                Text("영차영차..")
+            } label: {
+                Text("Add Button")
+            }
+
+        }
+    }
+    
+    func removeList(at offsets: IndexSet) {
+        sharedData.remove(atOffsets: offsets)
+    }
+    
+    func moveList(from source: IndexSet, to destination: Int) {
+        sharedData.move(fromOffsets: source, toOffset: destination)
+    }
+    
+    func addPlainItem() {
+        //TODO: - 토글x, 기본매듭 추가시
+    }
+    
+    func addDisclosureGroupItem() {
+        //TODO: - 토글 버튼있는 응용매듭 추가시
     }
 }
 
