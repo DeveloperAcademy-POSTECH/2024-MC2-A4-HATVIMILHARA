762eda272ec6fa72ef6fae8e2970b601ca50b58a
daminoworld <ekadls757@gmail.com>
2024-05-20 16:57:43 +0900

    Chore: #1-Add Models

---
+++ Dorae/Dorae/Model/Knot.swift
@@ -0,0 +1,40 @@
+//
+//  Knot.swift
+//  Dorae
+//
+//  Created by Damin on 5/20/24.
+//
+
+import Foundation
+
+
+
+enum 매듭 {
+    case 기본(매듭: 기본), 응용(매듭: 응용), 길이(매듭: 길이), 끈목(매듭: 끈목), 술(매듭: 술)
+}
+
+struct 기본 {
+    let 매듭이름: String
+    let 매듭이미지: String
+    var 귀: [String] = []
+}
+
+struct 응용 {
+    let 매듭이름: String
+    let 매듭이미지: String
+    var 귀: [String] = []
+    var 하위매듭리스트: [기본] = []
+}
+
+struct 길이 {
+    var 끈길이: Float
+}
+
+enum 끈목 {
+    case 극세사, 세사, 중사, 중세사, 태사, 극테사
+}
+
+enum 술 {
+    case 딸기술, 봉술, 방망이술, 낙지발술, 방울술, 전복술, 금전지술, 잔술
+
+}
---
+++ Dorae/Dorae/Model/Pattern.swift
@@ -0,0 +1,16 @@
+//
+//  Pattern.swift
+//  Dorae
+//
+//  Created by Damin on 5/20/24.
+//
+
+import Foundation
+
+
+
+struct 도안 {
+    var 매듭리스트: [매듭]
+    let 생성일자: String
+    let 제목: String
+}
