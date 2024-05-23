ce5fa6347eed4ecbfb708e631a7c4d926d2339b3
ye-gang <yegang13209@naver.com>
2024-05-23 15:52:25 +0900

    Fix: #22-모델 수정에 따른 더미 데이터 수정

--- Dorae/Dorae/Resources/PatternDummy.swift
+++ Dorae/Dorae/Resources/PatternDummy.swift
@@ -8,23 +8,23 @@
 import Foundation
 
 final class PatternDummy {
-    static let 도안배열: [도안] = [
-        도안(매듭리스트: [
-            .기본(매듭: 도래매듭),
-            .기본(매듭: 귀도래매듭),
-            .응용(매듭: 항아리매듭)
-        ],생성일자: "2000.01.11", 제목: "첫 번째 도안"),
+    static let patternList: [Pattern] = [
+        Pattern(knotList: [
+            .basic(knot: 도래매듭),
+            .basic(knot: 귀도래매듭),
+            .applied(knot: 항아리매듭)
+        ],createdAt: "2000.01.11", title: "첫 번째 도안"),
         
-        도안(매듭리스트: [
-            .기본(매듭: 도래매듭),
-            .기본(매듭: 귀도래매듭),
-            .응용(매듭: 항아리매듭)
-        ],생성일자: "2010.02.22", 제목: "두 번째 도안"),
+        Pattern(knotList: [
+            .basic(knot: 도래매듭),
+            .basic(knot: 귀도래매듭),
+            .applied(knot: 항아리매듭)
+        ],createdAt: "2010.02.22", title: "두 번째 도안"),
         
-        도안(매듭리스트: [
-            .기본(매듭: 도래매듭),
-            .기본(매듭: 귀도래매듭),
-            .응용(매듭: 항아리매듭)
-        ],생성일자: "2020.03.33", 제목: "세 번째 도안"),
+        Pattern(knotList: [
+            .basic(knot: 도래매듭),
+            .basic(knot: 귀도래매듭),
+            .applied(knot: 항아리매듭)
+        ],createdAt: "2020.03.33", title: "세 번째 도안"),
     ]
 }
