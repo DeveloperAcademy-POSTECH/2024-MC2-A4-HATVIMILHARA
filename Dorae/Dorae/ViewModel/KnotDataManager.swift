c8d207e288ff2803255958c8cfead8af3e14a10a
daminoworld <ekadls757@gmail.com>
2024-05-23 23:37:34 +0900

    Feat: #29-Add KnotDataManager Observable class
    - KnotDataManager를 @Observable로 만들고 도안에서 공유할 Knot리스트를 생성
    - getKnotName이라는 Knot를 받아서 각 케이스에 따른 Knot의 이름을 리턴해주는 메서드 추가
    - getBasicKnotData라는 Knot를 받아서 basicKnot일때 BasicKnot 정보를 리턴해는 메서드 추가

--- Dorae/Dorae/ViewModel/KnotDataManager.swift
+++ Dorae/Dorae/ViewModel/KnotDataManager.swift
@@ -7,4 +7,38 @@
 
 import Foundation
 
-
+@Observable
+final class KnotDataManager {
+    /// 줄글 도안과 이미지 도안에서 참조할 Knot 리스트
+    var knotList: [Knot] = []
+    
+    func getKnotName(knot: Knot) -> String {
+        var knotName = "이름없음"
+        switch knot {
+        case .basic(let knot):
+            knotName = knot.knotName.rawValue
+        case .applied(let knot):
+            knotName = knot.knotName.rawValue
+        case .etc(let knot):
+            if let knot = knot.braid {
+                knotName = "끈목"
+            }else if let knot = knot.interval {
+                knotName = "간격"
+            }else if let knot = knot.tassel {
+                knotName = "술"
+            }
+        }
+        return knotName
+    }
+    
+    func getBasicKnotData(knot: Knot) -> BasicKnot? {
+        var basicKnot: BasicKnot?
+        switch knot {
+        case .basic(let knot):
+            basicKnot = knot
+        default:
+            break
+        }
+        return basicKnot
+    }
+}
