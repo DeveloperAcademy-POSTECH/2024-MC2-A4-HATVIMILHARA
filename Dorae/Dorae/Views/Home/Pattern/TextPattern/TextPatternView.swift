6b30bc858fa7487171fef1d9a1a103b70ba2a51f
ye-gang <yegang13209@naver.com>
2024-05-24 17:19:35 +0900

    Chore: #44-TextPatternView 수정(오스틴)-기본매듭 기능추가

--- Dorae/Dorae/Views/Home/Pattern/TextPattern/TextPatternView.swift
+++ Dorae/Dorae/Views/Home/Pattern/TextPattern/TextPatternView.swift
@@ -11,9 +11,11 @@ struct TextPatternView: View {
     // 홈뷰에서 가져오는 첫 번째 Pattern 타입 knotList
     @State var knotList = PatternDummy.patternList[0].knotList
     @State private var braid = "" //끈목
-    @State private var textfiled = ""
-//    @State private var textFields: [String] = []
-    
+    @State private var loopTextfiled = ""
+    @State private var intervalTextfiled = ""
+    @State private var textfiled = [String]()
+    let 매듭1 = 도래매듭
+    let 매듭2 = 도래매듭
     var body: some View {
         NavigationStack {
             VStack {
@@ -65,42 +67,87 @@ struct TextPatternView: View {
         switch knot {
         case .basic(let knot):
             if let loop = knot.loop, !loop.isEmpty {
-                DisclosureGroup("\(knot.knotName.rawValue)") {
+                DisclosureGroup(knot.knotName.rawValue) {
                     //TODO: - 빈 String 배열 만들어서 인덱스값이랑 엮기
-                    ForEach(0..<loop.count, id: \.self) { index in
-                        HStack {
-                            Text("귀")
-                            Image(systemName: "\(index+1).circle")
-                            TextField("cm", text: $textfiled)
+//                    ForEach(0..<loop.count, id: \.self) { index in
+//                        HStack {
+//                            Text("귀")
+//                            Image(systemName: "\(index+1).circle")
+//                            TextField("cm", text: $loopTextfiled)
+//                        }
+//                    }
+                    LoopListView(loop: loop) { loop in
+                        knotList = knotList.map { knotItem in
+                            if case .basic(let data) = knotItem {
+                                if data.id == knot.id {
+                                    var tempBasicKnot = data
+                                    tempBasicKnot.loop = loop
+                                    let newBasicKnot: Knot = .basic(knot: tempBasicKnot)
+                                    return newBasicKnot
+                                } else {
+                                    return knotItem
+                                }
+                            }
+                            return knotItem
                         }
                     }
                 }
             } else {
-                Text(knot.knotName.rawValue)
+                HStack {
+                    Image(BasicKnotName.도래매듭.rawValue)
+                    Text(knot.knotName.rawValue)
+                }
             }
             
         case .applied(let knot):
             //TODO: - 1. 개수만 있는 애들, 2. 귀만 있는 애들, 3. 귀, 개수 둘 다 있는 애들, 4. 기본만 있는 애들
-            DisclosureGroup("\(knot.knotName)") {
-                
-            }
-            
+//            Text(knot.knotName.rawValue)
+            DisclosureGroup(
+                content: {
+                    // 내용들
+                }, label: {
+                    HStack {
+                        Image("공작매듭")
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                        Text(knot.knotName.rawValue)
+                    }
+                })
+
         case .etc(let knot):
-            if let braid = knot.braid {
-                Text(braid) // 끈목임
-            } else if let interval = knot.interval {
+            if let interval = knot.interval {
                 HStack {
-                    Image(systemName: "1.circle")
+                    Image("간격")
                     Text("간격")
+                    TextField("간격(cm)을 입력해주세요.", text: $intervalTextfiled)
                 }
             } else if let lasso = knot.lasso {
                 HStack {
-                    Image(systemName: "3.circle")
-                    Text(lasso)
+                    Image("고")
+                    Text("\(lasso) 고 임")
                 }
             } else {
                 Text("없는 애임")
             }
+        }
+    }
+}
+
+fileprivate struct LoopListView: View {
+    @State var loop: [String] = []
+    var changeLoop: ((_ loop: [String]) -> Void)
+    
+    var body: some View {
+        VStack {
+            ForEach($loop, id: \.self) { $text in
+                HStack {
+                    Text("귀")
+                    TextField("cm", text: $text)
+                }
+            }
+        }
+        .onChange(of: loop) { oldValue, newValue in
+            changeLoop(newValue)
         }
     }
 }
