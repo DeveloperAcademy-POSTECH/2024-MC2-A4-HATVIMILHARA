eff7f034110f24a041db05d166bca0a0d5c698e1
daminoworld <ekadls757@gmail.com>
2024-05-24 16:01:05 +0900

    Add: #45-Add EtcKnot global constant
    - 기타 매듭용 EtcKnot를 만들어서 고, 술, 간격 전역상수 추가

---
+++ Dorae/Dorae/Resources/KnotList/EtcKnot.swift
@@ -0,0 +1,12 @@
+//
+//  EtcKnot.swift
+//  Dorae
+//
+//  Created by Damin on 5/24/24.
+//
+
+import Foundation
+
+let 고 = EtcKnot(lasso: "고")
+let 술 = EtcKnot(tassel: "술")
+let 간격 = EtcKnot(interval: "")
