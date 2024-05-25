f2be5dcbe74eb871b16292b7d354594e71aba2fb
한인우 <ninnu@MacBook-Pro-de-han-in-u.local>
2024-05-25 20:13:09 +0900

    Feat: #67- 스플래시 기능 추가

---
+++ Dorae/Splash/PlayerViewModel.swift
@@ -0,0 +1,29 @@
+//
+//  PlayerViewModel.swift
+//  Dorae
+//
+//  Created by 한인우 on 5/25/24.
+//
+
+import AVKit
+import SwiftUI
+
+class PlayerViewModel: ObservableObject {
+    @Published var isVideoFinished = false
+    var player: AVPlayer?
+
+    init() {
+        if let url = Bundle.main.url(forResource: "SplashVideo", withExtension: "mp4") {
+            self.player = AVPlayer(url: url)
+            NotificationCenter.default.addObserver(self, selector: #selector(playerDidFinishPlaying), name: .AVPlayerItemDidPlayToEndTime, object: self.player?.currentItem)
+        }
+    }
+
+    @objc func playerDidFinishPlaying() {
+        DispatchQueue.main.async {
+            self.isVideoFinished = true
+        }
+    }
+}
+
+
