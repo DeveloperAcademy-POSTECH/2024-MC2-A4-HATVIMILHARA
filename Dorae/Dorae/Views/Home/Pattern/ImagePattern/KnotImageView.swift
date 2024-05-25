e845b0e1208a039b042572119fdd57979b060db2
daminoworld <ekadls757@gmail.com>
2024-05-26 02:06:34 +0900

    Feat: #63-Create Size calculation method
    - Knot카데고리 별로 boundingSize에 대한 높이 비율을 곱해서 리턴해주는 메서드를 만들어 사용
    - 이미지에서 실제 그림이 있는 부분만 자른 boundingBox의 height 크기에 0.3을 곱하여 가장 큰 공작매듭까지 다 그려질 수 있도록 수정

--- Dorae/Dorae/Views/Home/Pattern/ImagePattern/KnotImageView.swift
+++ Dorae/Dorae/Views/Home/Pattern/ImagePattern/KnotImageView.swift
@@ -13,7 +13,6 @@ struct KnotImageView: View {
     
     let knot: Knot
     let index: Int
-    let containerSize: CGSize
     var body: some View {
         Group {
             if let image = UIImage(named: knotDataManager.getKnotName(knot: knot)) {
@@ -23,9 +22,8 @@ struct KnotImageView: View {
                         Image(uiImage: image)
                             .resizable()
                             .aspectRatio(contentMode: .fit)
-                            .frame(width: min(boundingBox.width, containerSize.width),
-                                   height: min(boundingBox.height, containerSize.height))
-                            .clipped()
+                            .frame(width: boundingBox.height,
+                                   height: getHeight(for: knot, boundingBox: boundingBox))
                             .onAppear {
                                 print("\(knotDataManager.getKnotName(knot: knot)) bounding boxSize: \(boundingBox.size)")
                                 print("인덱스", index)
@@ -33,22 +31,47 @@ struct KnotImageView: View {
                                 imagePatternViewModel.checkSizeCalFinished(knotList: knotDataManager.knotList)
                             }
                             .onChange(of: knotDataManager.knotList) { _, _ in
-                                imagePatternViewModel.offsetYDict = [:]
                                 setKnotSizeDict(knot: knot, boundingBox: boundingBox)
                                 imagePatternViewModel.checkSizeCalFinished(knotList: knotDataManager.knotList)
-
                             }
+                        /// Image
+                            .border(.white, width: 5)
                     }
+                    /// VStack
+//                    .frame(width: geometry.size.width)
                     .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .border(.yellow, width: 3)
                 }
-                .frame(height: boundingBox.height)
+                /// geometry
+                .frame(width: boundingBox.height)
+                .frame(height: getHeight(for: knot, boundingBox: boundingBox))
+                .border(.red, width: 3)
+
+
             } else {
                 Text("Image not found")
             }
         }
     }
+
+       private func getHeight(for knot: Knot, boundingBox: CGRect) -> CGFloat {
+           switch knot {
+           case .basic:
+               return boundingBox.height * 0.3
+           case .applied(let knot):
+//               if knot.knotName == 지게매듭.knotName || knot.knotName == 스타매듭.knotName {
+//                   return boundingBox.height * 0.5
+//               }else if knot.knotName == 공작매듭.knotName {
+//                   return boundingBox.height * 0.3
+//               }
+//               return boundingBox.height * 0.7
+               return boundingBox.height * 0.3
+           case .etc:
+               return boundingBox.height * 0.3
+           }
+       }
     
-    func setKnotSizeDict(knot: Knot, boundingBox: CGRect) {
+    private func setKnotSizeDict(knot: Knot, boundingBox: CGRect) {
         var bottomHeightRatio: CGFloat = 0
         var bottomWidthRatio: CGFloat = 1
         var topHeightRatio: CGFloat = 0
