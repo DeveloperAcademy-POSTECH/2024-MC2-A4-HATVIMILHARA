a13384856bf946af946f78ec21ae75e408c9b187
jiyoungsophia <sophiathedev@gmail.com>
2024-05-28 15:01:24 +0900

    Chore: #95-날짜 관련 데이터 수정

--- Dorae/Dorae/Extensions/Date+.swift
+++ Dorae/Dorae/Extensions/Date+.swift
@@ -8,20 +8,10 @@
 import Foundation
 
 extension Date {
-    
     func toString() -> String {
         let dateFormatter = DateFormatter()
         dateFormatter.locale = Locale(identifier: "ko_KR")
         dateFormatter.dateFormat = "yyyy.MM.dd"
         return dateFormatter.string(from: self)
-    }
-}
-
-extension String {
-    func StringToDate() -> Date? {
-        let dateFormatter = DateFormatter()
-        dateFormatter.locale = Locale(identifier: "ko_KR")
-        dateFormatter.dateFormat = "yyyy.MM.dd"
-        return dateFormatter.date(from: self)
     }
 }
