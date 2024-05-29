d59e0c4afdc7f48a133eabaed93e0858d4d112b3
jiyoungsophia <sophiathedev@gmail.com>
2024-05-29 16:09:00 +0900

    Chore: #110- 그림도안 top에 padding값 추가

--- Dorae/Dorae/Views/Home/Pattern/ImagePattern/ImagePatternView.swift
+++ Dorae/Dorae/Views/Home/Pattern/ImagePattern/ImagePatternView.swift
@@ -11,18 +11,15 @@ import CoreGraphics
 
 struct ImagePatternView: View {
     @State private var imagePatternVM = ImagePatternViewModel()
-    
     @Bindable var pattern: Pattern
     
     var body: some View {
         VStack {
-                VStack {
-                    ForEach(Array(pattern.knotList.enumerated()), id: \.offset) { idx, knot in
-                        KnotImageView(imagePatternViewModel: imagePatternVM, knot: knot, index: idx)
-                            .offset(y: imagePatternVM.offsetYDict[idx] ?? 0)
-                    }
-                }
+            ForEach(Array(pattern.knotList.enumerated()), id: \.offset) { idx, knot in
+                KnotImageView(imagePatternViewModel: imagePatternVM, knot: knot, index: idx)
+                    .offset(y: imagePatternVM.offsetYDict[idx] ?? 0)
+            }
         }
-           
+        .padding(.top, 24)
     }
 }
