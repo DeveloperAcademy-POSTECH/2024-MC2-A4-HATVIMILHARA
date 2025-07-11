1afd75a126c0679ab8de3fd5949ccef2b91fcb88
jiyoungsophia <sophiathedev@gmail.com>
2025-07-12 01:48:59 +0900

    Chore: #145- 패딩값 수정

--- Dorae/Dorae/Views/Home/Pattern/KnotList/KnotButtonListView.swift
+++ Dorae/Dorae/Views/Home/Pattern/KnotList/KnotButtonListView.swift
@@ -19,48 +19,51 @@ struct KnotButtonListView: View {
     ]
     
     var body: some View {
-        ScrollView(showsIndicators: false) {
-            LazyVGrid(columns: columns, spacing: 16) { ///상하간격
-                ForEach(knotNameList, id: \.self) { knotName in
-                    KnotButton(knotName: knotName)
-                        .onTapGesture {
-                            var newKnot: Knot = Knot.basic(knot: 도래매듭)
-                            switch selectedTab {
-                            case .basicCategory:
-                                basicKnotCollection.forEach { basicKnot in
-                                    if knotName == basicKnot.knotName.rawValue {
-                                        let newBasicKnot = BasicKnot(basicKnot: basicKnot)
-                                        newKnot = Knot.basic(knot: newBasicKnot)
+        GeometryReader { geometry in
+            ScrollView(showsIndicators: false) {
+                LazyVGrid(columns: columns, spacing: 16) { ///상하간격
+                    ForEach(knotNameList, id: \.self) { knotName in
+                        KnotButton(knotName: knotName)
+                            .onTapGesture {
+                                var newKnot: Knot = Knot.basic(knot: 도래매듭)
+                                switch selectedTab {
+                                case .basicCategory:
+                                    basicKnotCollection.forEach { basicKnot in
+                                        if knotName == basicKnot.knotName.rawValue {
+                                            let newBasicKnot = BasicKnot(basicKnot: basicKnot)
+                                            newKnot = Knot.basic(knot: newBasicKnot)
+                                        }
+                                    }
+                                case .appliedCategory:
+                                    appliedKnotCollection.forEach { appliedKnot in
+                                        if knotName == appliedKnot.knotName.rawValue {
+                                            let newAppliedKnot = AppliedKnot(appliedKnot: appliedKnot)
+                                            newKnot = Knot.applied(knot: newAppliedKnot)
+                                        }
+                                    }
+                                case .etcCategory:
+                                    if knotName == EtcKnotName.고.rawValue {
+                                        newKnot = Knot.etc(knot: EtcKnot(lasso: knotName))
+                                    } else if knotName == EtcKnotName.술.rawValue {
+                                        newKnot = Knot.etc(knot: EtcKnot(tassel:  knotName))
+                                    } else {
+                                        newKnot = Knot.etc(knot: EtcKnot(interval:  0.0))
+                                    }
+                                case .templateCategory:
+                                    if let templatePattern = Pattern.predefinedTemplates.first(where: { $0.title == knotName }) {
+                                        // 템플릿 패턴의 매듭들을 현재 패턴에 추가
+                                        pattern.knotList.append(contentsOf: templatePattern.knotList)
+                                        try? modelContext.save()
                                     }
                                 }
-                            case .appliedCategory:
-                                appliedKnotCollection.forEach { appliedKnot in
-                                    if knotName == appliedKnot.knotName.rawValue {
-                                        let newAppliedKnot = AppliedKnot(appliedKnot: appliedKnot)
-                                        newKnot = Knot.applied(knot: newAppliedKnot)
-                                    }
-                                }
-                            case .etcCategory:
-                                if knotName == EtcKnotName.고.rawValue {
-                                    newKnot = Knot.etc(knot: EtcKnot(lasso: knotName))
-                                } else if knotName == EtcKnotName.술.rawValue {
-                                    newKnot = Knot.etc(knot: EtcKnot(tassel:  knotName))
-                                } else {
-                                    newKnot = Knot.etc(knot: EtcKnot(interval:  0.0))
-                                }
-                            case .templateCategory:
-                                if let templatePattern = Pattern.predefinedTemplates.first(where: { $0.title == knotName }) {
-                                    // 템플릿 패턴의 매듭들을 현재 패턴에 추가
-                                    pattern.knotList.append(contentsOf: templatePattern.knotList)
-                                    try? modelContext.save()
-                                }
+                                pattern.knotList.append(newKnot)
+                                try? modelContext.save()
                             }
-                            pattern.knotList.append(newKnot)
-                            try? modelContext.save()
-                        }
+                    }
                 }
+                .padding(.horizontal, max(16, min(24, (geometry.size.width - 184) / 2)))
+                .padding(.vertical, 24)
             }
-            .padding(24) /// 백그라운드와 그리드 사이 패딩값
         }
         .frame(maxWidth: .infinity)
     }
