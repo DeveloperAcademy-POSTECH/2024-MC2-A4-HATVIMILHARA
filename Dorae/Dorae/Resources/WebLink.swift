8011672ac50c0a9b4d4a2e53c9f23a8d52010ec0
ye-gang <yegang13209@naver.com>
2025-07-23 17:17:18 +0900

    Feat: #146-WebLink 추가

---
+++ Dorae/Dorae/Resources/WebLink.swift
@@ -0,0 +1,15 @@
+//
+//  WebLink.swift
+//  Dorae
+//
+//  Created by yegang on 7/13/25.
+//
+
+import Foundation
+
+// 한국어, 일본어, 그 외 영어로 할거면 페이지 링크 다 따로 달아야 됨
+enum WebLinks {
+    static let krInfoNotion = URL(string: "https://www.notion.so/22fd0d11dc6d808aa138d34ff2165561")
+    static let jaInfoNotion = URL(string: "https://www.naver.com/")
+    static let enInfoNotion = URL(string: "https://www.google.com/")
+}
