968e6b24d0fcbf7871ed21dbc8991e85eaebb5d2
ye-gang <yegang13209@naver.com>
2025-07-25 16:26:59 +0900

    Chore: #146-나라별 인포 페이지 링크 수정

--- Dorae/Dorae/Resources/WebLink.swift
+++ Dorae/Dorae/Resources/WebLink.swift
@@ -9,7 +9,7 @@ import Foundation
 
 // 한국어, 일본어, 그 외 영어로 할거면 페이지 링크 다 따로 달아야 됨
 enum WebLinks {
-    static let krInfoNotion = URL(string: "https://www.notion.so/22fd0d11dc6d808aa138d34ff2165561")
-    static let jaInfoNotion = URL(string: "https://www.naver.com/")
-    static let enInfoNotion = URL(string: "https://www.google.com/")
+    static let krInfoNotion = URL(string: "https://incredible-light-74e.notion.site/Dorae-9969cf80fd764b6abe9bc39689ab29ce")
+    static let jaInfoNotion = URL(string: "https://incredible-light-74e.notion.site/Dorae-23bd0d11dc6d8003a58be49b6666eb63")
+    static let enInfoNotion = URL(string: "https://incredible-light-74e.notion.site/Dorae-English-23bd0d11dc6d809ba5acf00d7839cc29")
 }
