2ec6a4e19d976d3653b55b24434e75824d06ddd0
ye-gang <yegang13209@naver.com>
2025-08-07 00:20:58 +0900

    Add: #154-AppDelegate 파일 추가

--- Dorae/Dorae/AppDelegate.swift
+++ Dorae/Dorae/AppDelegate.swift
@@ -5,3 +5,14 @@
 //  Created by yegang on 8/6/25.
 //
 
+import Mixpanel
+import UIKit
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        Mixpanel.initialize(token: PrivateKey.getMixpanel ?? "", trackAutomaticEvents: false)
+        Mixpanel.mainInstance().useIPAddressForGeoLocation = false
+        return true
+    }
+}
